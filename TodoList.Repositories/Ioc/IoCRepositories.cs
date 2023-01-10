43544d371431e43ed6b7f3e1e79c2b9010bf088e
werikorus <werik.msti@gmail.com>
2023-01-10 20:17:08 -0300

    mais alterações para adicioncar campos de relação com o idUser

--- TodoList.Repositories/Ioc/IoCRepositories.cs
+++ TodoList.Repositories/Ioc/IoCRepositories.cs
@@ -13,14 +13,13 @@ public static class IoCRepositories
     public static IServiceCollection AddDbContext(this IServiceCollection services, IConfiguration configuration)
         => services.AddDbContext<TodoListContext>(dbcontextoptions
             => dbcontextoptions.UseLazyLoadingProxies()
-                .UseSqlite(configuration.GetConnectionString("DefaultConnection"), sqliteOptions 
-                => sqliteOptions.MigrationsAssembly(typeof(TodoListContext).Assembly.GetName().Name)));
+                .UseSqlite(configuration.GetConnectionString("DefaultConnection"), sqliteOptions
+                    => sqliteOptions.MigrationsAssembly(typeof(TodoListContext).Assembly.GetName().Name)));
 
     public static void AddRepository(this IServiceCollection services)
     {
         services.AddScoped<IUserRepository, UserRepository>();
         services.AddScoped<IListRepository, ListRepository>();
         services.AddScoped<ITaskListRepository, TaskListRepository>();
-    } 
-
+    }
 }
