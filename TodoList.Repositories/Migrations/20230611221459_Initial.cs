76fb95b19c151ced1c0697b5bfd59fe690025b97
werik.cunha <werik.cunha@softplan.com.br>
2024-04-03 17:59:21 -0300

    Files modified by lenovo pc

---
+++ TodoList.Repositories/Migrations/20230611221459_Initial.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoList.Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "List",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    DescriptionList = table.Column<string>(type: "TEXT", nullable: false),
+                    IdUser = table.Column<Guid>(type: "TEXT", nullable: false),
+                    DateCreate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DateUpdate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_List", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TaskList",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    IdList = table.Column<Guid>(type: "TEXT", nullable: false),
+                    IdUser = table.Column<Guid>(type: "TEXT", nullable: false),
+                    DescriptionTask = table.Column<string>(type: "TEXT", nullable: false),
+                    Done = table.Column<bool>(type: "INTEGER", nullable: false),
+                    DateCreate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DateUpdate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskList", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Password = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Role = table.Column<string>(type: "TEXT", nullable: false),
+                    UrlAvatar = table.Column<string>(type: "TEXT", nullable: false),
+                    DateCreate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DateUpdate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "List");
+
+            migrationBuilder.DropTable(
+                name: "TaskList");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
